9ccf90fa2c19d798319e86ee3d6931ed73d2e38d
Keunoh <rmsdhcjswo1@gmail.com>
2023-01-15 22:21:20 +0900

    스프링은 타입을 자동 컨버팅

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,2 +1,26 @@
-package hello.typeconverter.controller;public class HelloController {
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자타입으로 조회가됨
+        Integer intValue = Integer.valueOf(data); //숫자타입으로 변경해야함
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        System.out.println("data.type = " + data.getClass());
+        return "ok";
+    }
 }
